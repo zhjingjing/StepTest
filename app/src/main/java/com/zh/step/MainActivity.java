fc9e770b24d6abdd83870b592f036540f67c75c1
zhaohang <zhaohang@zhizhangyi.com>
2021-01-12 11:00:14 +0800

    计步器完善

--- app/src/main/java/com/zh/step/MainActivity.java
+++ app/src/main/java/com/zh/step/MainActivity.java
@@ -1,16 +1,24 @@
 package com.zh.step;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
@@ -24,34 +32,30 @@ import static android.content.Intent.ACTION_SCREEN_ON;
 import static android.content.Intent.ACTION_USER_PRESENT;
 
 public class MainActivity extends AppCompatActivity {
-    private final static String TAG = "MainActivity";
+    private final static String TAG = "MainActivity2";
     SensorManager manager;
-    private Sensor stepCountSensor;
-    private Sensor stepDetectorSensor;
-    private Sensor accSensor;
+    private Sensor sensor;
     private float stepCount;
     private TextView textView;
+    private TextView tvDesc;
     private float lastCount = 0;//上次关机后存储的值。
     private long lastTime;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         manager = (SensorManager) getSystemService(SENSOR_SERVICE);
         textView = findViewById(R.id.tv_step);
-        stepCountSensor = manager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
-        stepDetectorSensor = manager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        accSensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-//        stepSensor = manager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        tvDesc = findViewById(R.id.tv_step_desc);
 
-        manager.registerListener(new SensorListener(), stepCountSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        manager.registerListener(new SensorListener(), stepDetectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        manager.registerListener(new SensorListener(), accSensor, SensorManager.SENSOR_DELAY_NORMAL);
-//        } else {
-//            Toast.makeText(this, "不支持计步器", Toast.LENGTH_LONG).show();
-//        }
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BODY_SENSORS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BODY_SENSORS, Manifest.permission.ACTIVITY_RECOGNITION}, 1101);
+        } else {
+            registerStepListener();
+        }
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(Intent.ACTION_BOOT_COMPLETED);
         intentFilter.addAction(Intent.ACTION_SHUTDOWN);
@@ -60,61 +64,51 @@ public class MainActivity extends AppCompatActivity {
         intentFilter.addAction(Intent.ACTION_USER_PRESENT);
         registerReceiver(new MyBroadCast(), intentFilter);
 
-//        handler.postAtTime(testRunnable, "action", SystemClock.uptimeMillis() + 1000);
-//        handler.postAtTime(testRunnable2, "action", SystemClock.uptimeMillis() + 1000);
-//
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                handler.removeCallbacksAndMessages("action");
-//            }
-//        }, 10000);
     }
 
-    private class SensorListener implements SensorEventListener{
+    public void registerStepListener() {
+        sensor = manager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        tvDesc.setText("当前是Step counter 传感器：\n记录的是开机后的总步数，延迟<=10s ");
+        if (sensor == null) {
+            sensor = manager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+            tvDesc.setText("当前是Step detector 传感器：\n每次步数+1，延迟<=2s ");
+        }
+        if (sensor == null) {
+            sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            tvDesc.setText("当前是加速度传感器 ：\n每次步数+1，");
+        }
 
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
-                //TYPE_STEP_COUNTER event.values[0]为计步历史累加值
-                // TYPE_STEP_DETECTOR event.values[0]=1.0f
-                if (event.values != null) {
-                    stepCount = event.values[0];
-                    Log.e(TAG, "onSensorChanged: 当前步数：" + stepCount);
-                    textView.setText("onSensorChanged: 当前步数：" + stepCount);
-                } else {
-                    Log.e(TAG, "event.values == null");
-                }
-            }else if (event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR){
-                Log.e(TAG, "TYPE_STEP_DETECTOR");
-            }else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-                final float alpha = 0.8f;
-
-                float x=event.values[0];
-                float y=event.values[1];
-                float z=event.values[2];
-                Log.e(TAG, "x="+x+ " y="+y+" z="+z);
+        StepSensorListener stepEventListener = new StepSensorListener();
+        stepEventListener.initListener((count, sensorType) -> {
+            switch (sensorType) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    stepCount += count;
+                    Log.d(TAG, "TYPE_ACCELEROMETER  onSensorChanged: 当前步数：" + stepCount);
+                    textView.setText("当前步数：" + stepCount);
+                case Sensor.TYPE_STEP_DETECTOR:
+                    stepCount += count;
+                    Log.d(TAG, "TYPE_STEP_DETECTOR  onSensorChanged: 当前步数：" + stepCount);
+                    textView.setText("当前步数：" + stepCount);
+                    break;
+                case Sensor.TYPE_STEP_COUNTER:
+                    stepCount = count;
+                    Log.d(TAG, "TYPE_STEP_COUNTER  onSensorChanged: 当前步数：" + stepCount);
+                    textView.setText("当前步数：" + stepCount);
+                    break;
             }
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
+        });
+        manager.registerListener(stepEventListener, sensor, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-    public boolean isSupportStep() {
-        List<Sensor> list = manager.getSensorList(Sensor.TYPE_ALL);
-        for (Sensor sensor : list) {
-            Log.e(TAG, "initData: " + sensor.getName());
-            if (sensor.getName().equals("step counter")) {
-                return true;
-            }
+        if (requestCode == 1101 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            registerStepListener();
         }
-        return false;
-    }
 
+    }
 
     private int flag = -1;
 
@@ -189,5 +183,17 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, "test2");
             handler.postAtTime(testRunnable2, "action", SystemClock.uptimeMillis() + 1000);
         }
+    }
+
+    /**
+     * @return 是否支持传感器
+     */
+    private boolean isSupportBodySensor(Context context) {
+        PackageManager pm = context.getPackageManager();
+        boolean isSupport = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT
+                && (pm.hasSystemFeature(PackageManager.FEATURE_SENSOR_STEP_COUNTER)
+                || pm.hasSystemFeature(PackageManager.FEATURE_SENSOR_STEP_DETECTOR));
+        Log.e(TAG, String.valueOf(isSupport));
+        return isSupport;
     }
 }
---
+++ app/src/main/java/com/zh/step/StepCountListener.java
@@ -0,0 +1,11 @@
+package com.zh.step;
+
+/**
+ * @Author: Administrator
+ * @Time 2021/1/8 0008 14:23
+ * @Email: zhaohang@zhizhangyi.com
+ * @Describe:
+ */
+public interface StepCountListener {
+    void stepCount(float count, int sensorType);
+}
